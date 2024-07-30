d442c35e9f10ea51f4bc5662df5d56f6ae259856
rafael <calixxtorafell@gmail.com>
2024-07-29 23:58:57 -0300

    configuration-of-project

--- backend/src/main/java/com/dsenvolvendosistemas/chat_app/config/AppConfig.java
+++ backend/src/main/java/com/dsenvolvendosistemas/chat_app/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -23,14 +24,14 @@ public class AppConfig {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeHttpRequests(authorize->authorize.requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll()
-                ).addFilterBefore(null, null)
+                ).addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .csrf().disable()
                 .cors().configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
                         CorsConfiguration cfg = new CorsConfiguration();
-                        cfg.setAllowedOrigins(ArrayStoreException.asList(
+                        cfg.setAllowedOrigins(Arrays.asList(
                                 "http:localhost:3000/"
                         ));
                         cfg.setAllowedMethods(Collections.singletonList("*"));
