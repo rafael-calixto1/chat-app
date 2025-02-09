3654fc6e538c0fc1a58c3afd3f767bab007f8262
rafael <calixxtorafell@gmail.com>
2025-02-09 16:32:10 -0300

    feat(auth): add JWT token provider and enhance security configuration
    Added TokenProvider, SecurityConstant, and updated exception handling. Modified UserRepository and UpdateUserRequest for security improvements.

---
+++ backend/src/main/java/com/dsenvolvendosistemas/chat_app/config/SecurityConstant.java
@@ -0,0 +1,9 @@
+package com.dsenvolvendosistemas.chat_app.config;
+
+public class SecurityConstant {
+
+    public static final String JWT_KEY="kzjjbeiurbZGyurZvzpaqekmeecfeeljliuogcerwmqzsduphbeheb";
+    public static final String HEADER = "Authorization";
+
+
+}
