f7f71c74b659e704258e0f1d41f4dffa27406481
rafael <calixxtorafell@gmail.com>
2025-02-15 18:12:33 -0300

    add RealTimeChat

--- backend/src/main/java/com/dsenvolvendosistemas/chat_app/ChatAppApplication.java
+++ backend/src/main/java/com/dsenvolvendosistemas/chat_app/ChatAppApplication.java
@@ -1,13 +1,22 @@
 package com.dsenvolvendosistemas.chat_app;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatAppApplication {
-
 	public static void main(String[] args) {
+		// Carregar variáveis do arquivo .env
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("SERVER_PORT", dotenv.get("SERVER_PORT"));
+		System.setProperty("URL_DATABASE", dotenv.get("URL_DATABASE"));
+		System.setProperty("USERNAME_DB", dotenv.get("USERNAME_DB"));
+		System.setProperty("PASS_DB", dotenv.get("PASS_DB"));
+		System.setProperty("HIBERNATE_DDL_AUTO", dotenv.get("HIBERNATE_DDL_AUTO"));
+		System.setProperty("SHOW_SQL", dotenv.get("SHOW_SQL"));
+
+		// Iniciar a aplicação Spring Boot
 		SpringApplication.run(ChatAppApplication.class, args);
 	}
-
 }
